a0f167395542096a269116dec9e0a56af694993d
James Inkster <jinkster@myseneca.ca>
2019-11-25 20:52:54 -0500

    metastuffadded

--- src/app/meta-location/meta-location.component.ts
+++ src/app/meta-location/meta-location.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FileService } from '../file.service';
 
 @Component({
   selector: 'app-meta-location',
@@ -7,9 +8,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MetaLocationComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(files: File[]) { }
 
   ngOnInit() {
-  }
 
+  }
+   city: boolean;
+   prov: boolean;
+   delim: string;
+   Front: boolean;
+
+
+  addLocation(fileNames): void {
+
+    fileNames.forEach(element => {
+      if (this.city) {
+        if (this.Front) {
+          element.name = element.location + this.delim + element.name;
+        }
+        else {
+          let temp = element.split(".");
+          temp[0] += (this.delim + element.location + temp[1]);
+          element.name = temp[0];
+        }
+      }
+      if (this.prov) {
+        let province;
+        if (element['y-resolution'] > 2000){
+          province = "Ontario"
+        }
+        else {
+          province = "Quebec"
+        }
+        if (this.Front) {
+          element.name = province + this.delim + element.name;
+        }
+        else {
+          let temp = element.split(".");
+          temp[0] += (this.delim + province + temp[1]);
+          element.name = temp[0];
+        }
+      }
+    })
+
+  }
 }
+
--- src/app/meta-time/meta-time.component.ts
+++ src/app/meta-time/meta-time.component.ts
@@ -12,4 +12,50 @@ export class MetaTimeComponent implements OnInit {
   ngOnInit() {
   }
 
+  timeFormat : string;
+  location: boolean;
+
+  addDate(fileNames): void {
+    fileNames.forEach(element => {
+      if (element.date) {
+        //"2019-01-16"
+        let currT = element.date.split("T");
+        let curr = currT[1].split(":")
+        let HH = curr[0];
+        let MM = curr[1];
+        let SS = curr[2]
+        let cur_time;
+        switch (this.timeFormat) {
+          case "HH:MM:SS":
+            if (location)
+            cur_time = HH + "-" + MM + "-" + SS + "_";
+            else {
+              cur_time            }
+            break;
+          case "HH:MM":
+            if (location) {
+              cur_time = HH + "-" + MM + "_"
+            }
+            else {
+              cur_time = "_" + HH + "-" + MM;
+            }
+            break;
+
+        }
+        if (location) {
+          element.name = cur_time + element.name;
+        }
+        else {
+          var temp = element.name.split(".")
+          temp[0] += (cur_time + temp[1]);
+          element.name = temp[0];
+        }
+      }
+    }
+
+    )
+  }
+
+
+
 }
