e4af869df0531f77217dea09917eab85245cf7a0
Neil Ong <neil.ong31@gmail.com>
2019-11-28 20:51:10 -0500

    Started TS for b4/after

--- src/app/add-beforeafter/add-beforeafter.component.ts
+++ src/app/add-beforeafter/add-beforeafter.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+import { CustomFile } from '../file';
+import { FileService } from '../file.service';
 
 @Component({
   selector: 'app-add-beforeafter',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class AddBeforeafterComponent implements OnInit {
+  
+  ogFiles: CustomFile[];
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private m: FileService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.ogFiles = this.m.originalFiles;
+  }
+
+  OnSubmit(){
+
   }
 
 }
