12c7f06a90721e4fbcbd804034c9b01a55c154b5
James Inkster <jinkster@myseneca.ca>
2019-12-04 20:36:01 -0500

    fix #ur mother

--- src/app/file.service.ts
+++ src/app/file.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CustomFile } from './file';
-import _ from "lodash" 
+import _ from "lodash"
 import mockFiles from '../assets/MOCK_DATA.json';
 
 @Injectable({
@@ -9,6 +9,7 @@ import mockFiles from '../assets/MOCK_DATA.json';
 export class FileService {
   originalFiles: CustomFile[];
   modifiedFiles: CustomFile[];
+
   constructor() {
     this.originalFiles = [...mockFiles];
     this.modifiedFiles = _.cloneDeep(this.originalFiles);
--- src/app/meta-resolution/meta-resolution.component.ts
+++ src/app/meta-resolution/meta-resolution.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+import { CustomFile } from '../file';
+import { FileService } from '../file.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-meta-resolution',
@@ -6,10 +11,37 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class MetaResolutionComponent implements OnInit {
+  modFiles: CustomFile[];
+  beforeOrAfter: boolean;
+  filename: Boolean;
+  ext: Boolean;
 
-  constructor() { }
+  constructor( private m: FileService) { }
 
   ngOnInit() {
+    this.modFiles = [];
+    this.beforeOrAfter = true;
+  }
+
+  onSubmit(f: NgForm) {
+    for(let i = 0; i < this.m.modifiedFiles.length; i++){
+      let temp = String(this.m.modifiedFiles[i].name);
+      let varshanchoice = this.m.modifiedFiles[i]["x-resolution"];
+      let varshansmells = this.m.modifiedFiles[i]["y-resolution"];
+
+        if(this.beforeOrAfter){
+          temp = this.m.modifiedFiles[i].name + '_' + varshanchoice + "x" + varshansmells;
+        }
+        else{
+          temp = varshanchoice + "x" + varshansmells + '_' + this.m.modifiedFiles[i].name;
+        }
+
+      var modFile = this.m.modifiedFiles[i];
+      modFile.name = temp;
+      this.modFiles.push(modFile);
+    }
+    this.m.modifiedFiles = this.modFiles;
+    console.log(this.m.modifiedFiles);
   }
 
 }
